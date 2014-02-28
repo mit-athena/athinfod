5c11f6888e3f31482d03f484772b718d1d43c8ab
Jonathan Reed <jdreed@mit.edu>
2014-02-28 11:12:51 -0500

    Rename script; add setup.py
    - Rename athinfod.py to athinfod, since both distutils and dh are
      incapable of renaming files while installing them, even in 2014.
    - Add a setup.py for the aforementioned distutils

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='athinfod',
+      version='10.2',
+      author='Debathena Project',
+      author_email='debathena@mit.edu',
+      scripts=['athinfod'])
